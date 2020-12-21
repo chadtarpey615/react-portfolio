9fba9c70d38d81c33364267fcf4f7c4028ae08b3
Chad Tarpey <chadtarpey@gmail.com>
2020-12-21 17:09:35 -0600

    Worked on portfolio page and compnents

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,12 +1,14 @@
 import React from "react";
-import image from "../images/sky.jpeg"
+import image from "../images/IMG_6139.jpeg"
 
 export default function About() {
     return (
         <main>
-            <img src={image} alt="Sky" className="absolute object-cover w-full h-full"/>
+            <div className="box-border h-22 w-22 p-4 flex flex-row ">
+            <img src={image} alt="Sky" className="absolute justify-center object-contain w-full h-full"/>
+            </div>
             <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
-                <h1 className="text 6xl text-red-100 font-bold leading-none lg: leading-snug home-name">Hello. I'm Chad</h1>
+                <h1 className="text 6xl text-red-100 font-bold leading-none lg: leading-snug home-name"></h1>
             </section>
         </main>
     )
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,5 +1,98 @@
 import React from "react";
+import { SocialIcon } from "react-social-icons"
+import image from "../images/project1.png"
+import image1 from "../images/time-zone-traveler.png"
+import image2 from "../images/weather.png"
+
 
 export default function Portfolio() {
-    return <h1>Portfolio Page!</h1>
+    return (
+        <main>
+            <div className="container">
+                <h1 className="text-5xl text-center py-2">My Projects</h1>
+                <div className="flex space-x-4">
+                    <div className="flex-1">
+                        <h1 className="text-3xl">Super Hero Search</h1>
+                        <p>My first group project in class. The subject of this project is wrapped around Marvel
+                        & DC superHeroes.Using three api calls to get stats, gifs, and movies onto the screen that is created
+                        dynamically
+                        with JQUERY
+                        </p>
+                        <SocialIcon c
+                            url="https://eatandplay.herokuapp.com"
+                            className="mr-4"
+                            target="_blank"
+                            fgColor="#fff"
+                            style={{ height: 35, width: 35 }} />
+            click here to see deployed app
+            <br />
+                        <SocialIcon
+                            url="https://github.com/chadtarpey615/Time-Zone-Traveler"
+                            className="mr-4"
+                            target="_blank"
+                            fgColor="#fff"
+                            style={{ height: 35, width: 35 }} />
+            click here to see github repo
+                    </div>
+                    <div className="flex-1">
+                        <img className="grid-cols-3" src={image} />
+                    </div>
+
+                </div>
+                <div className="flex space-y-4">
+                    <div className="flex-1">
+                        <h1 className="text-3xl">Time Zone Traveler</h1>
+                        <p>Time Zone Traveler application that uses three api's. Map Box, Open cage, and Algolia api's.
+                        With GSAP animations for the intro loading of the home page. You can add food locations all over the world .
+                        When selecting the add food location button it with take you to food form page. From there you fill out all
+                        the fields to input a new food location into MySql database using sequelize models. Then a food icon will appear on
+  the map that you can click on that with display information for that restaurant including a link to that restaurant website.</p>
+                        <SocialIcon c
+                            url="https://eatandplay.herokuapp.com"
+                            className="mr-4"
+                            target="_blank"
+                            fgColor="#fff"
+                            style={{ height: 35, width: 35 }} />
+            click here to see deployed app
+            <br />
+                        <SocialIcon
+                            url="https://github.com/chadtarpey615/Time-Zone-Traveler"
+                            className="mr-4"
+                            target="_blank"
+                            fgColor="#fff"
+                            style={{ height: 35, width: 35 }} />
+            click here to see github repo
+                    </div>
+                    <div className="flex-1">
+                        <img className="grid-cols-2" src={image1} />
+                    </div>
+                </div>
+                <div className="flex space-y-4">
+                    <div className="flex-1">
+                        <h1 className="text-3xl">Super Hero Search</h1>
+                        <p>First project into class</p>
+                        <SocialIcon c
+                            url="https://eatandplay.herokuapp.com"
+                            className="mr-4"
+                            target="_blank"
+                            fgColor="#fff"
+                            style={{ height: 35, width: 35 }} />
+            click here to see deployed app
+            <br />
+                        <SocialIcon
+                            url="https://github.com/chadtarpey615/Time-Zone-Traveler"
+                            className="mr-4"
+                            target="_blank"
+                            fgColor="#fff"
+                            style={{ height: 35, width: 35 }} />
+            click here to see github repo
+                    </div>
+                    <div className="flex-1">
+                        <img className="grid-cols-2" src={image2} />
+                    </div>
+                </div>
+            </div>
+        </main>
+    )
 }
+
